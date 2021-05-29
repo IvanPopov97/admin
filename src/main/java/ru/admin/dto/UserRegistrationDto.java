8ef61cd455365a9fdcffbead4ba8eea2cfb91d8f
popov_id <p77599478113@yandex.ru>
2021-05-29 21:04:23 +0500

    Незначительные исправления ошибок. Изменил расширение файлов freemarker

--- src/main/java/ru/admin/dto/UserRegistrationDto.java
+++ src/main/java/ru/admin/dto/UserRegistrationDto.java
@@ -1,4 +1,20 @@
 package ru.admin.dto;
 
-public class UserRegistrationDto extends UserAuthorizationDto {
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import ru.admin.validation.annotation.Password;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class UserRegistrationDto {
+    @Schema(description = "Адрес электронной почты пользователя")
+    @Email(message = "не похоже на реальный адрес электронной почты")
+    @NotBlank(message = "не должно быть пустым")
+    private String email;
+
+    @Schema(description = "Пароль пользователя")
+    @Password
+    private String password;
 }
