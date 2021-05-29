d0b0c063d04bacbbf6ea6fd1b249fce64004728c
popov_id <p77599478113@yandex.ru>
2021-05-30 03:25:18 +0500

    Избавился от дублирования в шаблонах. Сделал, чтобы логин можно было вызвать из vue

--- src/main/java/ru/admin/config/SecurityConfig.java
+++ src/main/java/ru/admin/config/SecurityConfig.java
@@ -2,20 +2,28 @@ package ru.admin.config;
 
 import org.passay.*;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.WebFilterExchange;
+import reactor.core.publisher.Mono;
 import ru.admin.config.properties.MinCountProperties;
 import ru.admin.config.properties.PasswordGenerationProperties;
 import ru.admin.config.properties.PasswordProperties;
 import ru.admin.enitity.UserRole;
 import ru.admin.service.UserDetailsService;
 import ru.admin.utils.PasswordToolBuilder;
+
+import static org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers.pathMatchers;
 
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
@@ -36,9 +44,11 @@ public class SecurityConfig {
                 .pathMatchers("/doc/**", "/webjars/swagger-ui/**", "/v3/api-docs/**", "/actuator/**", "/admin/**").hasRole(UserRole.ADMIN.name())
                 .anyExchange().authenticated()
                 .and()
-                .httpBasic()
-                .and()
-                .formLogin().loginPage("/login")
+                .formLogin()
+                .loginPage("/login")
+                .requiresAuthenticationMatcher(pathMatchers(HttpMethod.POST, "/login"))
+                .authenticationSuccessHandler(this::onAuthenticationSuccess)
+                .authenticationFailureHandler(this::onAuthenticationError)
                 .and()
                 .build();
         // @formatter:on
@@ -50,7 +60,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    PasswordProperties passwordProperties () {
+    PasswordProperties passwordProperties() {
         return new PasswordProperties();
     }
 
@@ -83,5 +93,15 @@ public class SecurityConfig {
     PasswordGeneratorTemplate passwordGeneratorTemplate() {
         PasswordGenerationProperties generation = passwordProperties().getGeneration();
         return passwordToolBuilder().buildGenerator(generation.getMinLength(), generation.getMaxLength());
+    }
+
+    private Mono<Void> onAuthenticationSuccess(WebFilterExchange exchange, Authentication authentication) {
+        exchange.getExchange().getResponse().setStatusCode(HttpStatus.OK);
+        return Mono.empty();
+    }
+
+    private Mono<Void> onAuthenticationError(WebFilterExchange exchange, AuthenticationException e) {
+        exchange.getExchange().getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        return Mono.empty();
     }
 }
