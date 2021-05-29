bbb0e2e8b6370e451a02cf54d62a59c993c54a9f
popov_id <p77599478113@yandex.ru>
2021-05-29 22:05:25 +0500

    Избавился от лишних аннотаций в конфигурации реактора

--- src/main/java/ru/admin/config/BlockingCallDetectionConfig.java
+++ src/main/java/ru/admin/config/BlockingCallDetectionConfig.java
@@ -1,25 +1,15 @@
 package ru.admin.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.blockhound.BlockHound;
 import reactor.tools.agent.ReactorDebugAgent;
 import ru.admin.config.properties.BlockingCallDetectionProperties;
 
-import javax.annotation.PostConstruct;
-
 @Configuration
 public class BlockingCallDetectionConfig {
-    @Bean
-    public BlockingCallDetectionProperties blockingCallDetectionProperties () {
-        return new BlockingCallDetectionProperties();
-    }
-
-    @SuppressWarnings("unused")
-    @PostConstruct
-    public void installBlockHound() { // устанавливает инструмент для поиска "блокирующих" участков кода
+    public BlockingCallDetectionConfig(BlockingCallDetectionProperties blockingCallDetectionProperties) {
         ReactorDebugAgent.init();
-        if (blockingCallDetectionProperties().isEnable()) {
+        if (blockingCallDetectionProperties.isEnable()) {
             // отключаем проверки для Swagger (Swagger в production лучше отключить)
             BlockHound.builder()
                     .allowBlockingCallsInside("org.springframework.core.io.buffer.DataBufferUtils", "readByteChannel")
