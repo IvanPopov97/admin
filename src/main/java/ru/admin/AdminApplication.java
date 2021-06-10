39ee0f6f737f4b42f290438467533d93f44e62a7
popov_id <p77599478113@yandex.ru>
2021-06-10 23:46:21 +0500

    Попытка исправить локализацию сообщений валидации

--- src/main/java/ru/admin/AdminApplication.java
+++ src/main/java/ru/admin/AdminApplication.java
@@ -4,14 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-import java.util.Locale;
-
 @SpringBootApplication
 @ConfigurationPropertiesScan(basePackages = "ru.admin.config.properties")
 public class AdminApplication {
 
     public static void main(String[] args) {
-        Locale.setDefault(new Locale("ru", "RU"));
         SpringApplication.run(AdminApplication.class, args);
     }
 
